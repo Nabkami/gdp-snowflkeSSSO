82dc8525abb42f1eeacf7c60639885b8e866d82f
Nabkami <108486632+Nabkami@users.noreply.github.com>
2024-08-13 14:22:48 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,16 +2,5 @@ import pandas as pd
 import snowflake.connector
 import streamlit as st
 
-# Snowflake Connection
-def snowflake_conn():
-    return snowflake.connector.connect(**st.secrets["snowflake"])
-
-# Llamando a la función de conexión a Snowflake
-conn = snowflake_conn()
-
-# Ejecutando Consulta
-query = 'SELECT 1'
-# Leyendo los datos
-df = pd.read_sql(query, conn)
-# Mostrando los datos
-st.write(df)
+# Verificando las claves en st.secrets
+st.write("Available keys in st.secrets:", list(st.secrets.keys()))
